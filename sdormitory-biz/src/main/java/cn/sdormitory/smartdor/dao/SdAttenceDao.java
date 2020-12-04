94ad0e7cac0157063c3e5fae9e7c0e34521d4933
rt1102 <903109398@qq.com>
2020-12-04 11:05:26 +0800

    考勤信息实体

---
+++ sdormitory-biz/src/main/java/cn/sdormitory/smartdor/dao/SdAttenceDao.java
@@ -0,0 +1,21 @@
+package cn.sdormitory.smartdor.dao;
+
+import cn.sdormitory.smartdor.entity.SdAttence;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *  Created By ruanteng
+ *  DateTime：2020/11/27
+ */
+@Mapper
+public interface SdAttenceDao extends BaseMapper<SdAttence> {
+
+    List<SdAttence> getList(@Param("date")String date,@Param("currIndex")Integer currIndex,@Param("pageSize") Integer pageSize);
+}
