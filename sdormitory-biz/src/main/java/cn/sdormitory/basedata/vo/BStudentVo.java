f4b5fa5dc0e11d0c6bf0849046be7ab9ac9ea2b0
rt1102 <903109398@qq.com>
2020-12-05 15:31:17 +0800

    删除多余的代码

--- sdormitory-biz/src/main/java/cn/sdormitory/basedata/vo/BStudentVo.java
+++ sdormitory-biz/src/main/java/cn/sdormitory/basedata/vo/BStudentVo.java
@@ -1,11 +1,7 @@
 package cn.sdormitory.basedata.vo;
 
-import cn.sdormitory.common.constant.CommonConstant;
 import lombok.Data;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
  * Created By ruanteng
@@ -30,16 +26,5 @@ public class BStudentVo {
 
     private String photo;
 
-    public static void main(String[] args) throws ParseException {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
-        String str = simpleDateFormat.format(new Date())+" "+ CommonConstant.ATTENDANCE_TIME;
-        int i = simpleDateFormat.parse("2020-12-6 10:55:55").compareTo(simpleDateFormat2.parse(str));
-        System.out.println(i);
-
-
-
-
-    }
 }
